4161c538e118604babc60d48722108eb1352d147
jun229 <bjl229@cornell.edu>
2024-08-04 21:46:32 -0400

    - Initialized data preparation - Started NN model

--- main.py
+++ main.py
@@ -26,4 +26,14 @@ x_trainData = []
 y_trainData = []
 
 for x in range(prediction_days, len(scaled_data)):
-    x_trainData.append(scaled_data[])
+    x_trainData.append(scaled_data[x-prediction_days:x, 0])
+    y_trainData.append(scaled_data[x,0])
+
+x_trainData, y_trainData = np.array(x_trainData), np.array(y_trainData) #Create into numpy array
+x_trainData = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#Building The Model
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_trainData.shape[1], 1)))
+model.add(Dropout(0.2))
